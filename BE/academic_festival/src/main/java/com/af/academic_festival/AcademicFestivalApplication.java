7df0128f05f304d736726f4c322aa5c177394259
kingrangE <yuvw123@gmail.com>
2024-10-30 14:05:16 +0900

    학술제 시작
    1등 가져갈게요

---
+++ BE/academic_festival/src/main/java/com/af/academic_festival/AcademicFestivalApplication.java
@@ -0,0 +1,13 @@
+package com.af.academic_festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicFestivalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicFestivalApplication.class, args);
+    }
+
+}
---
+++ BE/academic_festival/src/test/java/com/af/academic_festival/AcademicFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.af.academic_festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicFestivalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
